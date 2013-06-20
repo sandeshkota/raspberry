2d679f459949f1092b8ed1c6cb9962f77b55223e
Sandesh kota <sandeshjkota@gmail.com>
2013-06-20 20:18:26 +0530

    Mission Raspberry

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raspberry::Application.config.secret_key_base = '056b9ad3d7de1c0a64dffb14b4e27f3feb01d07693e96d99d6e2de9871d1310fa0aea04dd0b327ba146d96483cbb696d0a3da7e0a2ca30f9d86ca44130e4d91d'
